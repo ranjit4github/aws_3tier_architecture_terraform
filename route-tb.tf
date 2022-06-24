a89283247736de6fefdd289642313a0823c40fbd
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2022-06-24 13:28:04 +0530

    Update route-tb.tf

--- route-tb.tf
+++ route-tb.tf
@@ -26,3 +26,17 @@ resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.public2.id
   route_table_id = aws_route_table.rtb.id
 }
+
+//Adding NAT Gateway into the default main route table
+resource "aws_default_route_table" "dfltrtb" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  tags = {
+    Name = "dfltrtb"
+  }
+}
