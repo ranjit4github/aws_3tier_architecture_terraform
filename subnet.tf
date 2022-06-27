3a4588ea923c1e1580d78998230e9e8f72b0e54c
Ranjit Swain <58227542+ranjit4github@users.noreply.github.com>
2022-06-27 17:11:29 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -24,17 +24,7 @@ resource "aws_subnet" "public" {
     Name = "public-sub"
   }
 }
-/*
-resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "ap-south-1b"
 
-  tags = {
-    Name = "public-sub"
-  }
-}
-*/
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.3.0/24"
